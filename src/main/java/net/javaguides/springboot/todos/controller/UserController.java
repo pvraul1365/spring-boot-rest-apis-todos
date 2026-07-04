cd529301dcd10b25dc7d231d727c8ea8ddd61ea6
Raúl Pérez Vicente <rperezv@ayesa.com>
2026-07-04 11:52:39 +0200

    156. Add Tag & Response for User Controller

--- src/main/java/net/javaguides/springboot/todos/controller/UserController.java
+++ src/main/java/net/javaguides/springboot/todos/controller/UserController.java
@@ -1,5 +1,6 @@
 package net.javaguides.springboot.todos.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -8,11 +9,13 @@ import net.javaguides.springboot.todos.entity.User;
 import net.javaguides.springboot.todos.request.PasswordUpdateRequest;
 import net.javaguides.springboot.todos.response.UserResponse;
 import net.javaguides.springboot.todos.service.UserService;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -33,18 +36,24 @@ public class UserController {
 
     private final UserService userService;
 
+    @Operation(summary = "User information", description = "Get current user info")
+    @ResponseStatus(HttpStatus.OK)
     @GetMapping("/info")
     public UserResponse getUserInfo() {
         log.info("🌐- UserController.getUserInfo() called");
         return userService.getUserInfo();
     }
 
+    @Operation(summary = "Delete user", description = "Delete current user account")
+    @ResponseStatus(HttpStatus.OK)
     @DeleteMapping
     public void deleteUser() {
         log.info("🌐️- UserController.deleteUser() called");
         userService.deleteUser();
     }
 
+    @Operation(summary = "Update password", description = "Change user password after verification")
+    @ResponseStatus(HttpStatus.OK)
     @PutMapping("/password")
     public void passwordUpdate(@Valid @RequestBody final PasswordUpdateRequest passwordUpdateRequest) {
         log.info("🌐️- UserController.password Update() called");
